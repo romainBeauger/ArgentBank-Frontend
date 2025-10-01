8a40d20e63b7614886eefeebf67d62cd74f3c251
romainBeauger <romain.beauger@gmail.com>
2025-10-01 17:05:58 +0200

    - Ajout du composant Footer et du css permettant de le positionner en bas de page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+      <p className="footer-text">Copyright 2020 Argent Bank</p>
+    </footer>
   )
 }
