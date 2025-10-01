dffb50e09250d317f87d6bb9a11b72182af72ed2
romainBeauger <romain.beauger@gmail.com>
2025-10-01 10:45:12 +0200

    - Creation composant Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import { BrowserRouter } from 'react-router-dom';
+import Header from '../src/components/Header';
+
 
 function App() {
 
 
   return (
     <>
+    <BrowserRouter>
+      <Header />
+    
+    </BrowserRouter>
       
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom"
+import logo from '../assets/argentBankLogo.png'
+
 export default function Header() {
+
   return (
-    <div>Header</div>
+
+    <nav className="main-nav">
+      <Link className="main-nav-logo" to="/">
+        <img 
+          className="main-nav-logo-image"
+          src={logo} 
+          alt="Argent Bank Logo" 
+          />
+          <h1 className="sr-only">Argent Bank</h1>
+      </Link>
+      <div>
+        <Link className="main-nav-item" to="/login">
+          <i className="main-nav-icon fa fa-user-circle"></i>
+          Sign in
+        </Link>
+      </div>
+    </nav>
   )
 }
